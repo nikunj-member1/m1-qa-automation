0fdd2755e68dd8e2e61948355b7d9634bf1fec16
nikunj-member1 <nikunj@member1.co>
2025-08-13 13:33:21 +0530

    Updated pom.xml

--- member1/src/test/java/com/member1/framework/common/Generics.java
+++ member1/src/test/java/com/member1/framework/common/Generics.java
@@ -22,12 +22,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.member1.framework.configurations.Configuration;
 
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.PointOption;
+
 
 public class Generics implements Configuration {
 
